a3a7b016bcb7c0db48d2b93f3495da578fc3ea3d
Rajiya Sultana <rssumu04@gmail.com>
2025-06-09 23:15:34 +0600

    Update PlayerMovement.cs

--- ObstacleDodge/Assets/Scripts/PlayerMovement.cs
+++ ObstacleDodge/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5;
     
     void Start()
     {
@@ -11,9 +12,9 @@ public class PlayerMovement : MonoBehaviour
     
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-
-        float zValue = Input.GetAxis("Vertical");
-        transform.Translate(xValue, 0f, zValue);
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(xValue, yValue, zValue);
     }
 }
