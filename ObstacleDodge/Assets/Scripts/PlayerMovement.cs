316c0c2c4da6b9307f283857dffc8e5610f343a6
Rajiya Sultana <rssumu04@gmail.com>
2025-06-11 10:52:11 +0600

    Update PlayerMovement.cs

--- ObstacleDodge/Assets/Scripts/PlayerMovement.cs
+++ ObstacleDodge/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,23 @@ public class PlayerMovement : MonoBehaviour
     
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!!");
+        Debug.Log("Click ASDW to move player..");
+        Debug.Log("Don't bump into any obstacle!!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float yValue = 0f;
