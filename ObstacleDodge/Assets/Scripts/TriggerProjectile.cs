1842b3af96002b5832049fe8bf40b22233ff45f3
Rajiya Sultana <rssumu04@gmail.com>
2025-06-13 00:16:04 +0600

    Added trigger for projectile

---
+++ ObstacleDodge/Assets/Scripts/TriggerProjectile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TriggerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject triggerProjectile;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            triggerProjectile.SetActive(true);
+        }
+    }
+}
