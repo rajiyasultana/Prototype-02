482133d77e1f4ac2661235a01c8d7ecc39c9d05d
Rajiya Sultana <rssumu04@gmail.com>
2025-06-20 22:36:51 +0600

    added reset button

--- ObstacleDodge/Assets/Scripts/Score.cs
+++ ObstacleDodge/Assets/Scripts/Score.cs
@@ -1,14 +1,50 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour
 {
-    int score = 1;
+    public int life = 3;
+    public TextMeshProUGUI lifeText;          
+    public GameObject gameOverPanel;
+
+
+    private void Start()
+    {
+        UpdateLifeUI();
+        gameOverPanel.SetActive(false);
+    }
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.tag != "Hit")
         {
-            Debug.Log("Your score is " + score);
-            score++;
+            life--;
+            Debug.Log("Your life is " + life);
+            UpdateLifeUI();
+
+            if (life <= 0)
+            {
+                GameOver();
+            }
         }
+    }
+
+    void UpdateLifeUI()
+    {
+        lifeText.text = "Life: " + life.ToString();
+    }
+
+    void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+
+        // Optional: Freeze the game
+        Time.timeScale = 0f;
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
